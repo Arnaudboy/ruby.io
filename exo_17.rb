8b98489dd4e3714011856ba2cb9de4679c26df4f
Arnaudboy <r.boyer895@laposte.net>
2019-07-09 15:00:00 +0200

    exo 17 fini

--- exo_17.rb
+++ exo_17.rb
@@ -4,18 +4,19 @@ i = naiss
 b = 0
 c = 0 
 
-while i <= 2017 
+while i <= 2017  
 	c = 2017 - i
 	puts i
+	
+	if c != b
+		puts "il y a #{c} ans, tu avais #{b} ans"
+		else 
+			puts "il y a #{c} ans, tu avais la moitié de ton âge"				
+		
+	end
 	i = i + 1
 	b = b + 1
 end
-
-	if 
-		puts "il y a #{c} ans, tu avais la moitié de ton âge"
-	else 		
-		puts "il y a #{c} ans, tu avais #{b} ans"		
-	end 
 
 
 
