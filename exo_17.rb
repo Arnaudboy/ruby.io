d6c92df3dfa9bfe3f83d17e916e7c8187ff55825
Arnaudboy <r.boyer895@laposte.net>
2019-07-09 23:11:37 +0200

    modification exo 17

--- exo_17.rb
+++ exo_17.rb
@@ -9,18 +9,12 @@ while i <= 2017
 	puts i
 
 	if b != c 
-		puts "il y a #{c} ans tu avais la moitié de ton âge"
-	else	
 		puts "il y a #{c} ans, tu avais #{b} ans"
+	else	
+		puts "il y a #{c} ans tu avais la moitié de ton âge"
 	end 
 
-	
-	if c != b
-		puts "il y a #{c} ans, tu avais #{b} ans"
-		else 
-			puts "il y a #{c} ans, tu avais la moitié de ton âge"				
-		
-	end
+
 
 	i = i + 1
 	b = b + 1
